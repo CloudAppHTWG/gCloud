0af895c5168f5fadf7f5fd0fd8195b69c9c22f6f
ChrisMythos <christoph.jurisch@weeb.de>
2022-10-23 17:51:26 +0200

    init script mod

--- gCloudCreateInstance.sh
+++ gCloudCreateInstance.sh
@@ -2,6 +2,6 @@
 gcloud compute instances create cloud-app-auto \
   --image-project=debian-cloud \
   --image-family=debian-10 \
-  --metadata-from-file=startup-script=/home/crispychris/Documents/initSript.sh
+  --metadata-from-file=startup-script=/workspaces/gCloud/initSript.sh
   --machine-type=e2-small
   --zone=europe-west3-c
